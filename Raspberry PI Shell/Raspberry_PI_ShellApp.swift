da5c942f2d296aa3e5028fdc3a3a87d6b0363723
boringx <perkeboring@gmail.com>
2023-10-17 19:13:24 +0300

    Initial Commit

---
+++ Raspberry PI Shell/Raspberry_PI_ShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raspberry_PI_ShellApp.swift
+//  Raspberry PI Shell
+//
+//  Created by Кирилл Иванов on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Raspberry_PI_ShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Raspberry PI ShellUITests/Raspberry_PI_ShellUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Raspberry_PI_ShellUITestsLaunchTests.swift
+//  Raspberry PI ShellUITests
+//
+//  Created by Кирилл Иванов on 17.10.2023.
+//
+
+import XCTest
+
+final class Raspberry_PI_ShellUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
