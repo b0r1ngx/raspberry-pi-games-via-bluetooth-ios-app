5cc6e15277ba933a0d07caad7b2e6edd33f10538
boringx <perkeboring@gmail.com>
2023-10-19 15:26:20 +0300

    Add Splash Screen to logic

--- Raspberry PI Shell/Components/BottomTabBar.swift
+++ Raspberry PI Shell/Components/BottomTabBar.swift
@@ -1,35 +1,37 @@
 import SwiftUI
 
 struct BottomTabBar: View {
-    @StateObject private var viewModel = DevicesViewModel.shared
+    @ObservedObject var viewModel: DevicesViewModel
     
     var body: some View {
-        TabView {
-            Games()
-                .tabItem {
-                    Image(systemName: "gamecontroller")
-                    Text("Games"~)
+            TabView {
+                Games()
+                    .tabItem {
+                        Image(systemName: "gamecontroller")
+                        Text("Games"~)
+                    }
+                Devices(viewModel: viewModel)
+                    .tabItem {
+                        // Solve with images, because additional drawings
+                        // or elements on tabItem is not reserved by SDK
+                        Image(
+                            viewModel.connectedTo != nil ? "bluetooth_green" : "bluetooth_red"
+                        )
+                        Text("Devices"~)
+                    }
+                Translation(
+                    translation: viewModel.translation
+                ).tabItem {
+                    Image("translate")
+                        .renderingMode(.template)
+                    Text("Translation"~)
                 }
-            Devices(viewModel: viewModel)
-                .tabItem {
-                    // Solve with images, because additional drawings
-                    // or elements on tabItem is not reserved by SDK
-                    Image(
-                        viewModel.connectedTo != nil ? "bluetooth_green" : "bluetooth_red"
-                    )
-                    Text("Devices"~)
-                }
-            Translation(
-                translation: viewModel.translation
-            ).tabItem {
-                Image("translate")
-                    .renderingMode(.template)
-                Text("Translation"~)
             }
-        }
     }
 }
 
 #Preview {
-    BottomTabBar()
+    BottomTabBar(
+        viewModel: DevicesViewModel.shared
+    )
 }
--- Raspberry PI Shell/Raspberry_PI_ShellApp.swift
+++ Raspberry PI Shell/Raspberry_PI_ShellApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct Raspberry_PI_ShellApp: App {
+    @StateObject private var viewModel = DevicesViewModel.shared
+    @State var isShowingSplash = true
+    
     var body: some Scene {
         WindowGroup {
-            BottomTabBar()
+            if isShowingSplash {
+                Splash().onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            isShowingSplash = false
+                        }
+                    }
+                }
+            } else {
+                BottomTabBar(viewModel: viewModel)
+            }
         }
     }
 }
